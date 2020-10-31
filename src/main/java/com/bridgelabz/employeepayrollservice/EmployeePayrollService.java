e9611545e4e2e1cca9fdadf1c2493aa6b5290ed8
Raj <rajkamal11126@gmail.com>
2020-10-31 11:18:56 +0530

    Added : demonstrate file operation & check file exist or not

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -7,12 +7,8 @@ import java.util.Scanner;
 public class EmployeePayrollService {
 	private List<EmployeePayrollData> employeePayrollList;
 
-	public EmployeePayrollService() {
-		this.employeePayrollList = employeePayrollList;
-	}
-
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
-
+		this.employeePayrollList = employeePayrollList;
 	}
 
 	public static void main(String[] args) {
@@ -22,7 +18,6 @@ public class EmployeePayrollService {
 		employeePayrollService.readEmployeePayrollData(scan);
 		employeePayrollService.writeEmployeePayrollData();
 	}
-
 
 	private void readEmployeePayrollData(Scanner consoleInputReader) {
 		System.out.println("Enter Employee ID: ");
